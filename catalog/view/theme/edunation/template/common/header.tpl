8406c6e4fbfbc04f195dbacef279a5f446512bc4
unknown <Greg@Greg-PC.(none)>
2012-04-12 17:05:40 -0400

    adding changes that you guys made to db

--- catalog/view/theme/edunation/template/common/header.tpl
+++ catalog/view/theme/edunation/template/common/header.tpl
@@ -66,9 +66,9 @@ DD_belatedPNG.fix('#logo img');
 
   <div id="welcome">
     <?php if (!$logged) { ?>
-    <?php echo $text_welcome; ?>
+    <?php //echo $text_welcome; ?>
     <?php } else { ?>
-    <?php echo $text_logged; ?>
+    <?php //echo $text_logged; ?>
     <?php } ?>
   </div>
   <div class="links"><a href="<?php echo $home; ?>"><?php echo $text_home; ?></a><a href="<?php echo $wishlist; ?>" id="wishlist-total"><?php echo $text_wishlist; ?></a><a href="<?php echo $account; ?>"><?php echo $text_account; ?></a><a href="<?php echo $shopping_cart; ?>"><?php echo $text_shopping_cart; ?></a><a href="<?php echo $checkout; ?>"><?php echo $text_checkout; ?></a></div>
---
+++ sulley_config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://sulley.dm.ucf.edu/~dig4530c_group06/v2/');
+define('HTTP_IMAGE', 'http://sulley.dm.ucf.edu/~dig4530c_group06/v2/image/');
+define('HTTP_ADMIN', 'http://sulley.dm.ucf.edu/~dig4530c_group06/v2/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://sulley.dm.ucf.edu/~dig4530c_group06/v2/');
+define('HTTPS_IMAGE', 'http://sulley.dm.ucf.edu/~dig4530c_group06/v2/image/');
+
+// DIR
+// ./
+define('DIR_APPLICATION', './catalog/');
+define('DIR_SYSTEM', './system/');
+define('DIR_DATABASE', './system/database/');
+define('DIR_LANGUAGE', './catalog/language/');
+define('DIR_TEMPLATE', './catalog/view/theme/');
+define('DIR_CONFIG', './system/config/');
+define('DIR_IMAGE', './image/');
+define('DIR_CACHE', './system/cache/');
+define('DIR_DOWNLOAD', './download/');
+define('DIR_LOGS', './system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dig4530c_group06');
+define('DB_PASSWORD', 'dig4530c_group06');
+define('DB_DATABASE', 'dig4530c_group06');
+define('DB_PREFIX', '');
+?>
